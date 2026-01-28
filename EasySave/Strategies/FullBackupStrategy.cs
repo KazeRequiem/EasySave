b7db759980fc15d4da6e6fd1926970035b6d1335
KazeRequiem <baptiste.noisette12@gmail.com>
2026-01-28 11:44:02 +0100

    feat: add backup interface, FullBackup and DifferentialBackup class

---
+++ EasySave/Strategies/FullBackupStrategy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave.Strategies
+{
+    public class FullBackupStrategy : IBackupStrategy
+    {
+        public void Execute(string SourcePath, string DestinationPath)
+        {
+            Directory.CreateDirectory(DestinationPath);
+            var dir = new DirectoryInfo(SourcePath);
+            foreach (var filePath in Directory.GetFiles(SourcePath))
+            {
+                var fileName = Path.GetFileName(filePath);
+                var destFile = Path.Combine(DestinationPath, fileName);
+                File.Copy(filePath, destFile, true);
+            }
+            DirectoryInfo[] subDirs = dir.GetDirectories();
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                string newDestinationPath = Path.Combine(DestinationPath, subDir.Name);
+                Execute(subDir.FullName, newDestinationPath);
+            }
+        }
+    }
+}
---
+++ EasySave/Strategies/IBackupStrategy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave.Strategies
+{
+    public interface IBackupStrategy
+    {
+        void Execute(string SourcePath, string DestinationPath);
+    }
+}
