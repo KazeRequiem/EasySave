cc7039f4082c8648932daf44dbe81ac61b5c5176
KazeRequiem <baptiste.noisette12@gmail.com>
2026-02-12 13:03:56 +0100

    feat: modified main view (the front was done by Ianice)

---
+++ EasySave.View/Views/ExecuteView.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using EasySave.Models;
+using EasySave.ViewModels;
+using EasySave.View.Resources;
+
+namespace EasySave.View.Views
+{
+    public partial class ExecuteView : UserControl
+    {
+        private MainViewModel viewModel;
+
+        public ExecuteView(MainViewModel sharedModel)
+        {
+            InitializeComponent();
+            this.viewModel = sharedModel;
+            CmbJobs.ItemsSource = viewModel.backupJobs;
+            CmbJobs.DisplayMemberPath = "name";
+        }
+
+        private async void BtnRunOne_Click(object sender, RoutedEventArgs e)
+        {
+            if (CmbJobs.SelectedItem is BackupJob selectedJob)
+            {
+                PbProgress.IsIndeterminate = true;
+                try
+                {
+                    await Task.Run(() => viewModel.ExecuteJob(selectedJob.id));
+                    MessageBox.Show(Strings.MsgJobDone, Strings.MsgSuccess, MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, Strings.MsgError, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                finally
+                {
+                    PbProgress.IsIndeterminate = false;
+                    PbProgress.Value = 100;
+                }
+            }
+            else
+            {
+                MessageBox.Show(Strings.MsgSelectJob, Strings.MsgWarning, MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private async void BtnRunAll_Click(object sender, RoutedEventArgs e)
+        {
+            if (viewModel.backupJobs == null || viewModel.backupJobs.Count == 0)
+            {
+                MessageBox.Show(Strings.MsgSelectJob, Strings.MsgWarning, MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            PbProgress.IsIndeterminate = true;
+
+            try
+            {
+                await Task.Run(() =>
+                {
+                    foreach (var job in viewModel.backupJobs)
+                    {
+                        viewModel.ExecuteJob(job.id);
+                    }
+                });
+
+                MessageBox.Show(Strings.MsgJobDone, Strings.MsgSuccess, MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Strings.MsgError, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                PbProgress.IsIndeterminate = false;
+                PbProgress.Value = 100;
+            }
+        }
+    }
+}
---
+++ EasySave.View/Views/JobView.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using EasySave.Models;
+using EasySave.ViewModels;
+using EasySave.View.Resources;
+
+namespace EasySave.View.Views
+{
+    public partial class JobView : UserControl
+    {
+        private MainViewModel viewModel;
+
+        public JobView(MainViewModel sharedModel)
+        {
+            InitializeComponent();
+            this.viewModel = sharedModel;
+            ChargerDonnees();
+        }
+
+        private void ChargerDonnees()
+        {
+            DgdJobs.ItemsSource = null;
+            DgdJobs.ItemsSource = viewModel.backupJobs;
+        }
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            var form = new JobForm();
+            if (form.ShowDialog() == true)
+            {
+                try
+                {
+                    viewModel.CreateJob(form.JobName, form.SourcePath, form.DestPath, form.SelectedType);
+                    ChargerDonnees();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, Strings.MsgError, MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void BtnModify_Click(object sender, RoutedEventArgs e)
+        {
+            if (DgdJobs.SelectedItem is BackupJob selectedJob)
+            {
+                var form = new JobForm(selectedJob.name, selectedJob.sourcePath, selectedJob.destinationPath, selectedJob.type);
+                if (form.ShowDialog() == true)
+                {
+                    try
+                    {
+                        viewModel.ModifyJob(selectedJob.id, form.JobName, form.SourcePath, form.DestPath, form.SelectedType);
+                        ChargerDonnees();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, Strings.MsgError, MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show(Strings.MsgSelectJob, Strings.MsgWarning, MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (DgdJobs.SelectedItem is BackupJob selectedJob)
+            {
+                var rep = MessageBox.Show(Strings.MsgConfirmDelete, Strings.MsgConfirmation, MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (rep == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        viewModel.DeleteJob(selectedJob.id);
+                        ChargerDonnees();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, Strings.MsgError, MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show(Strings.MsgSelectJob, Strings.MsgWarning, MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
